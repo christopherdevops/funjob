2ec0bed9ac84a5d531655fecd3ae523e13be0cc9
christopherdevops <snake10547@outlook.com>
2023-02-19 10:59:42 +0800

    implement get request on view

--- src/View/Helper/UserProfileHelper.php
+++ src/View/Helper/UserProfileHelper.php
@@ -24,12 +24,12 @@ class UserProfileHelper extends Helper
      * @return bool
      */
     public function isMyProfile() {
-        if (!$this->request->getSession()->check('Auth.User')) {
+        if (!$this->getView()->getRequest()->getSession()->check('Auth.User')) {
             return false;
         }
 
         // Usando $this->request->getParam('id') non funziona in /me
-        return $this->request->getSession()->read('Auth.User.id') == $this->_View->viewVars['User']->id;
+        return $this->getView()->getRequest()->getSession()->read('Auth.User.id') == $this->_View->viewVars['User']->id;
     }
 
 }
