de96696a29ff402f7f3267f18e17e9bd2dd09af9
christopherdevops <snake10547@outlook.com>
2023-02-20 08:59:38 +0800

    fix response type issue

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -148,7 +148,7 @@ class AppController extends Controller
     public function beforeRender(\Cake\Event\EventInterface $event)
     {
         if (!array_key_exists('_serialize', $this->viewBuilder()->getVars()) &&
-            in_array($this->response->type(), ['application/json', 'application/xml'])
+            in_array($this->response->getType(), ['application/json', 'application/xml'])
         ) {
             $this->set('_serialize', true);
         }
